38d66b82493b3a8d7a1b549242b6c3dda15ba535
valldek <valldek@gmail.com>
2019-11-02 10:26:18 +0100

    Rendering Elements

--- react-documentation/src/index.js
+++ react-documentation/src/index.js
@@ -7,4 +7,17 @@ import JSXIntro from '../main-concepts/JSXIntro';
 const root = document.getElementById('root');
 
 // ReactDOM.render(<HelloWorld />, root);
-ReactDOM.render(<JSXIntro />, root);
+// ReactDOM.render(<JSXIntro />, root);
+
+
+function tick () {
+  const element = (
+    <div>
+      <h1>Hello, world</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, root);
+}
+
+setInterval(tick, 1000);
