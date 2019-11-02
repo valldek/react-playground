833ef14e40ec9c9c8722198dc6e9fa1b034d95b2
valldek <valldek@gmail.com>
2019-11-02 08:47:03 +0100

    Hello World

---
+++ react-documentation/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from '../main-concepts/HelloWorld';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<HelloWorld />, root);
