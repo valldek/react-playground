b82abee20310d68b9b18f16adb02e0db0703367f
valldek <valldek@gmail.com>
2019-11-02 09:36:59 +0100

    JSX Intro

--- react-documentation/src/index.js
+++ react-documentation/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import HelloWorld from '../main-concepts/HelloWorld';
+import JSXIntro from '../main-concepts/JSXIntro';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<HelloWorld />, root);
+// ReactDOM.render(<HelloWorld />, root);
+ReactDOM.render(<JSXIntro />, root);
