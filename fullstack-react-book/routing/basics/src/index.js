6dfcaa01ec420bddf9561651044e05b742232f30
valldek <valldek@gmail.com>
2019-11-11 21:21:29 +0100

    Routing

---
+++ fullstack-react-book/routing/basics/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+import "./index.css";
+import "./App.css";
+
+import "./semantic-dist/semantic.min.css";
+
+// [STEP 1] Comment out this line:
+// import "./index-complete";
+
+// [STEP 2] Un-comment this line:
+ReactDOM.render(<App />, document.getElementById("root"));
