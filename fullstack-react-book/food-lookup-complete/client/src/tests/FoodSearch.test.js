ff11c7a55ba9bed19ce1f5867a9c2f769b2d648b
valldek <valldek@gmail.com>
2019-11-11 12:05:24 +0100

    API return no result test

--- fullstack-react-book/food-lookup-complete/client/src/tests/FoodSearch.test.js
+++ fullstack-react-book/food-lookup-complete/client/src/tests/FoodSearch.test.js
@@ -134,16 +134,28 @@ describe('FoodSearch', () => {
       });
 
       describe('then user types more', () => {
+        const value = 'broccx';
+
         beforeEach(() => {
-          // ... simulate user typing "x"
+          const input = wrapper.find('input').first();
+          input.simulate('change', {
+            target: { value: value },
+          });
         });
 
         describe('and API returns no results', () => {
           beforeEach(() => {
-            // ... simulate API returning no results
+            const secondInvocationArgs = Client.search.mock.calls[1];
+            const cb = secondInvocationArgs[1];
+            cb([]);
+            wrapper.update();
           });
 
-          // ... specs
+          it('should set the state property `foods`', () => {
+            expect(
+              wrapper.state().foods
+            ).toEqual([]);
+          });
         });
       });
     });
