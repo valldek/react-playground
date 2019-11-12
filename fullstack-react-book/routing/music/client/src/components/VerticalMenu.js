47cbb0b639d223bdc72bbcd9238878ce12e25ad6
valldek <valldek@gmail.com>
2019-11-12 13:18:58 +0100

    NavLink

--- fullstack-react-book/routing/music/client/src/components/VerticalMenu.js
+++ fullstack-react-book/routing/music/client/src/components/VerticalMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import '../styles/VerticalMenu.css';
 
@@ -9,15 +9,30 @@ const VerticalMenu = ({ albums, albumsPathname }) => (
       Albums
     </div>
     {
-      albums.map((album) => (
-        <Link
-          to={`${albumsPathname}/${album.id}`}
-          className='item'
-          key={album.id}
-        >
-          {album.name}
-        </Link>
-      ))
+      albums.map((album) => {
+        const to = `${albumsPathname}/${album.id}`;
+        return (
+          <NavLink
+            to={to}
+            className="item"
+            key={album.id}
+          >
+            {album.name}
+          </NavLink>
+        );
+
+
+        // const active = window.location.pathname === to;
+        // return (
+        //   <Link
+        //     to={to}
+        //     className={active ? 'item active' : 'item'}
+        //     key={album.id}
+        //   >
+        //     {album.name}
+        //   </Link>
+        // );
+      })
     }
   </div>
 );
