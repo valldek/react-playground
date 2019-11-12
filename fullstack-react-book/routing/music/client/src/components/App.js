19403914d65ff8b6789af0ffd9795c87bb6a66c6
valldek <valldek@gmail.com>
2019-11-12 13:29:24 +0100

    Redirect to proper path

--- fullstack-react-book/routing/music/client/src/components/App.js
+++ fullstack-react-book/routing/music/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import TopBar from './TopBar';
 import AlbumsContainer from './AlbumsContainer';
@@ -12,6 +12,15 @@ const App = () => (
     <div className='spacer row' />
     <div className='row'>
       <Route path='/albums' component={AlbumsContainer} />
+      <Route
+        exact
+        path='/'
+        render={
+          () => (
+            <Redirect to="/albums" />
+          )
+        }
+      />
     </div>
   </div>
 );
