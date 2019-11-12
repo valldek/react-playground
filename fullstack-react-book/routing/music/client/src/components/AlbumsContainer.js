31b116c6450c18f79013dc8f97691e6d37760ed0
valldek <valldek@gmail.com>
2019-11-12 13:05:48 +0100

    Path as props

--- fullstack-react-book/routing/music/client/src/components/AlbumsContainer.js
+++ fullstack-react-book/routing/music/client/src/components/AlbumsContainer.js
@@ -40,16 +40,32 @@ class AlbumsContainer extends Component {
         <div className='ui active centered inline loader' />
       );
     } else {
+      const matchPath = this.props.match.path;
       return (
         <div className='ui two column divided grid'>
           <div
             className='ui six wide column'
             style={{ maxWidth: 250 }}
           >
-            <VerticalMenu albums={this.state.albums} />
+            <VerticalMenu albums={this.state.albums} albumsPathname={matchPath} />
           </div>
           <div className='ui ten wide column'>
-            {
+            <Route
+              path={`${matchPath}/:albumId`}
+              render={ ({ match }) => {
+                const album = this.state.albums.find((val) => {
+                  return val.id === match.params.albumId
+                });
+
+                return (
+                  <Album
+                    album={album}
+                    albumsPathname={matchPath}
+                  />
+                )
+              }}
+            />
+            {/* {
               this.state.albums.map((a) => (
                 <div
                   className='row'
@@ -58,7 +74,7 @@ class AlbumsContainer extends Component {
                   <Album album={a} />
                 </div>
               ))
-            }
+            } */}
           </div>
         </div>
       );
